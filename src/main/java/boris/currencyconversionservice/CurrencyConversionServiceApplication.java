410381f2c0ea09ade174bac8c85d573b1f2bae87
borvil <31356001+borvil@users.noreply.github.com>
2019-02-07 16:04:25 +0100

    Update CurrencyConversionServiceApplication.java

--- src/main/java/boris/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/boris/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -3,6 +3,8 @@ package boris.currencyconversionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @EnableDiscoveryClient
 @EnableFeignClients("boris.currencyconversionservice")
